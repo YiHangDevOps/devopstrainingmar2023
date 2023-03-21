34f918090f481f4b016cb6181ae2039f15629631
YiHangDevOps <yi.hang.lim@intel.com>
2023-03-21 09:07:09 +0530

    user3 updated line17

--- CatalogAPI/Models/Catalog.cs
+++ CatalogAPI/Models/Catalog.cs
@@ -14,6 +14,7 @@ namespace CatalogAPI.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
+	//Catalog ID add length constraint update by user3
         [Column("Catalog_Id")]
         public long CatalogId { get; set; }
         [Column("Catalog_Name")]
